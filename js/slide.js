8ac184b4a3781df4073068c161acb86e8d0ac536
hiroka <mementoka@gmail.com>
2024-11-23 16:23:20 +0900

    add dot navigation

--- js/slide.js
+++ js/slide.js
@@ -1,31 +1,41 @@
-let currentSlide = 0;
+let currentSlideIndex = 0;
 let isSlideMoving = false;
 const track = document.querySelector(".carousel-track");
 const slides = document.querySelectorAll(".carousel-track a");
 const totalSlides = slides.length;
-const progressBar = document.querySelector(".progress-bar");
-const progressBars = document.querySelectorAll(".progress-bar");
 const transitionOffsetX = 100;
 const slideMoveMillis = 3000;
+const dots = document.querySelectorAll(".dot");
 
-setInterval(() => {
+let autoScroll = setInterval(() => {
   moveSlide(1);
 }, slideMoveMillis);
 
-function updateProgressBar() {
-    progressBars.forEach(bar => bar.classList.remove("active"));
-    progressBars[currentSlide].classList.add("active");
-}
+dots.forEach((_, dotIndex) => {
+  dot.addEventListener("click", () => {
+    moveSlide(dotIndex - currentSlideIndex);
+    updateDots(dotIndex);
+  });
+});
 
 function moveSlide(direction) {
   if (isSlideMoving) return;
   isSlideMoving = true;
-  currentSlide = (currentSlide + totalSlides + direction) % totalSlides;
+  currentSlideIndex = (currentSlideIndex + totalSlides + direction) % totalSlides;
 
-  const offset = -currentSlide * transitionOffsetX;
+  clearInterval(autoScroll);
+  const offset = -currentSlideIndex * transitionOffsetX;
   track.style.transform = `translateX(${offset}%)`;
-  updateProgressBar();
+  updateDots(currentSlideIndex);
   isSlideMoving = false;
+  autoScroll = setInterval(() => {
+    moveSlide(1);
+  }, slideMoveMillis);
+}
+
+function updateDots(index) {
+  dots.forEach(dot => dot.classList.remove('active'));
+  dots[index].classList.add('active');
 }
 
 moveSlide(0);
